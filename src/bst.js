e62223ad523640a330aaff23d507237a002832ca
Simone Schiavo <simoneschiavo@icloud.com>
2024-12-07 10:49:06 +0100

    Add the buildTree(array) method to Tree
    To support this I had to refactor the implementation and doing these
    main changes:
    - adding internal methods to remove duplicates, sort, and create BST

--- src/bst.js
+++ src/bst.js
@@ -1,14 +1,41 @@
 class Node {
-  constructor(value) {
-    this.value = value;
+  constructor(data) {
+    this.data = data;
     this.left = null;
     this.right = null;
   }
 }
 
 class Tree {
-  constructor(arr) {
-    this.arr = arr;
-    this.root = this.buildTree();
+  constructor(array) {
+    this.root = this.buildTree(array);
+  }
+
+  _removeDuplicates(array) {
+    return array.filter((value, index) => array.indexOf(value) === index);
+  }
+
+  _sortArray(array) {
+    return array.sort((a, b) => a - b);
+  }
+
+  _sortedArrayToBST(sortedArray, start, end) {
+    if (start > end) {
+      return null;
+    }
+
+    let mid = start + Math.floor((end - start) / 2);
+    let root = new Node(sortedArray[mid]);
+
+    root.left = this._sortedArrayToBST(sortedArray, start, mid - 1);
+    root.right = this._sortedArrayToBST(sortedArray, mid + 1, end);
+
+    return root;
+  }
+
+  buildTree(array) {
+    const uniqueValuesArray = this._removeDuplicates(array);
+    const sortedArray = this._sortArray(uniqueValuesArray);
+    return this._sortedArrayToBST(sortedArray, 0, sortedArray.length - 1);
   }
 }
