1bd8f422da8a521738141c659418a34a8380ad8a
Simone Schiavo <simoneschiavo@icloud.com>
2024-12-07 09:48:15 +0100

    Add the Tree class

--- src/bst.js
+++ src/bst.js
@@ -1,7 +1,13 @@
 class Node {
-    constructor(value) {
-        this.value = value;
-        this.left = null;
-        this.right = null;
-    }
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class Tree {
+  constructor() {
+    this.root = this.buildTree();
+  }
 }
